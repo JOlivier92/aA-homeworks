f731e3bf2b6c0abe01c41963e57c3bb3bc62de79
Joseph Olivier <josepholivier92@gmail.com>
2018-09-12 20:03:07 -0700

    Finish rails controllers views mailer homework

--- W4D3/UserStore/app/models/user.rb
+++ W4D3/UserStore/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :username, :session_token, presence: true
   validates :password_digest, presence: { message: 'Password can\'t be blank' }
   validates :password, length: { minimum: 6, allow_nil: true }
-  after_initialize :ensure_session_token
+  before_validation :ensure_session_token
 
   def password=(password)
     @password = password
